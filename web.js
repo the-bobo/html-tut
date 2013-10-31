934c0c0ff75fb10c5d5cb883233014bfa78ec79a
Bobo <the.bobo@gmail.com>
2013-10-31 11:29:29 -0400

    node file to serve our webpage

---
+++ web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+app.use(express.logger());
+
+var hello = fs.readFileSync("index.html").toString();
+
+app.get('/', function(request, response) {
+  response.send(hello);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
