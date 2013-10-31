8d3c10536aacd28c7b6ba4ef93d52fc3b89231a9
Bobo <the.bobo@gmail.com>
2013-10-31 12:30:16 -0400

    had to tweak express and use some path joining to get css to work

--- web.js
+++ web.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var app = express();
 app.use(express.logger());
+app.use(express.static(path.join(__dirname, 'public')));
 
 var hello = fs.readFileSync("index.html").toString();
 
